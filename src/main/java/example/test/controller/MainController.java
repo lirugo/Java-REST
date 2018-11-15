4d852f051e4f80b8ce9388e672da304fa044d44c
lirugo <roostik21@gmail.com>
2018-11-15 16:41:19 +0200

    Spring security & GAuth

---
+++ src/main/java/example/test/controller/MainController.java
@@ -0,0 +1,34 @@
+package example.test.controller;
+
+import example.test.domain.User;
+import example.test.repo.MessageRepo;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    private final MessageRepo messageRepo;
+
+    public MainController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
+
+    @GetMapping
+    public String main(Model model, @AuthenticationPrincipal User user){
+        HashMap<Object, Object> data = new HashMap<>();
+
+        data.put("profile", user);
+        data.put("messages", messageRepo.findAll());
+
+        model.addAttribute("frontendData", data);
+        return "index";
+    }
+
+}
